aa39a732c67a1e87eac49dba806f08122e6ace23
mariae777 <emeva777@gmail.com>
2024-10-03 21:34:45 -0500

    Add files via upload
    Taller 1. empleados y calculador Salario

---
+++ Empleados.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.empleados;
+
+/**
+ *
+ * @author Netec
+ */
+public class Empleados {
+    private String nombre;
+    private int edad;
+    public Empleados(String nombre, int edad){
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+    
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
